101c427d9ca297607d858a3fd09abbe714fa7d8b
Brett Allen <brettallen777@gmail.com>
2018-01-12 16:03:00 -0500

    Setup background

--- core/src/io/github/brettfx/bfxpong/BFXPong.java
+++ core/src/io/github/brettfx/bfxpong/BFXPong.java
@@ -3,31 +3,47 @@ package io.github.brettfx.bfxpong;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
 public class BFXPong extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
+	Texture texture;
+	Image img;
+	Stage stage;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+
+		texture = new Texture("background/backgroundzero.png");
+
+		img = new Image(texture);
+		img.setSize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+
+		Viewport viewport = new FitViewport(Gdx.graphics.getWidth(), Gdx.graphics.getHeight(),
+				new OrthographicCamera());
+		
+		stage = new Stage(viewport);
+		stage.addActor(img);
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		stage.draw();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		texture.dispose();
+		stage.dispose();
 	}
 }
